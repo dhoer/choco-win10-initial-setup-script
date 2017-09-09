4ff25295a7506decd4c49d368132cd52b5e258a8
Dennis Hoer <dennis.hoer@pearson.com>
2017-09-08 20:24:18 -0700

    First commit

---
+++ ReadMe.md
@@ -0,0 +1,50 @@
+﻿## Description
+
+This is a PowerShell script for automation of routine tasks done after
+fresh installations of Windows 10 and Windows Server 2016.
+
+https://github.com/Disassembler0/Win10-Initial-Setup-Script
+
+This script will download and execute and ApplyAll (default)
+and RestoreAll presets for OS type, e.g., WinServer2016-*All.preset.
+
+## Usage
+
+If you just want to run the ApplyAll preset:
+
+    choco install -y win10-initial-setup-script
+
+ or
+
+    choco install -y win10-initial-setup-script --params "'/ApplyAll'"
+
+Make sure your account is a member of *Administrators* group as the
+script attempts to run with elevated privileges.
+
+### Advanced usage
+
+To supply a customized preset, you can either pass the function names
+directly as parameters.
+
+    choco install -y win10-initial-setup-script --params "'/EnableFirewall /EnableDefender'"
+
+Or you can create a file where you write the function names (one
+function name per line, no commas, whitespaces allowed, comments on
+separate lines starting with `#`) and then pass the filename using
+*/Custom* parameter.
+
+Example of a preset file `mypreset.txt`:
+
+    @"
+    # Security tweaks
+    EnableFirewall
+    EnableDefender
+
+    # UI tweaks
+    ShowKnownExtensions
+    ShowHiddenFiles
+    "@ > C:/temp/mypreset.txt
+
+Command using the preset file above:
+
+    choco install -y win10-initial-setup-script --params "'/Custom:C:/temp/mypreset.txt'"
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,48 @@
+﻿$packageName    = $env:ChocolateyPackageName
+$packageVersion = $env:ChocolateyPackageVersion
+$url            = 'https://github.com/Disassembler0/Win10-Initial-Setup-Script/archive/2.7.zip'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$checksum       = '801da854786017ac8d6b45d6f276db23b8fc12075550afa7fb267595bc628a33'
+$checksumType   = 'sha256'
+
+Install-ChocolateyZipPackage $packageName $url $toolsDir -checksum $checksum -checksumType $checksumType
+
+$pp = Get-PackageParameters
+
+$server = (Get-CimInstance Win32_OperatingSystem).caption -like '*server*'
+
+. $toolsDir\tweaks.ps1
+
+if (($pp["applyall"] -eq $null -or $pp["applyall"] -eq '') -and ($pp["restoreall"] -eq $null -or $pp["restoreall"] -eq '') -and ($pp["custom"] -eq $null -or $pp["custom"] -eq '')) { $pp["applyall"] = $true }
+
+if ($pp["restoreall"] -eq $true) {
+  if ($server) {
+    $presetFile = "$toolsDir\WinServer2016-RestoreAll.preset"
+  } else {
+    $presetFile = "$toolsDir\Win10-RestoreAll.preset"
+  }
+  $tweaks = Get-Content $presetFile -ErrorAction Stop | ForEach { $_.Trim() } | Where { $_ -ne "" -and $_[0] -ne "#" }
+  $tweaks | ForEach { Invoke-Expression $_ }
+}
+
+if ($pp["applyall"] -eq $true) {
+  if ($server) {
+    $presetFile = "$toolsDir\WinServer2016-ApplyAll.preset"
+  } else {
+    $presetFile = "$toolsDir\Win10-ApplyAll.preset"
+  }
+  $tweaks = Get-Content $presetFile -ErrorAction Stop | ForEach { $_.Trim() } | Where { $_ -ne "" -and $_[0] -ne "#" }
+  $tweaks | ForEach { Invoke-Expression $_ }
+}
+
+if ($pp["custom"] -ne $null -and $pp["custom"] -ne '') {
+  $tweaks = Get-Content $pp["custom"] -ErrorAction Stop | ForEach { $_.Trim() } | Where { $_ -ne "" -and $_[0] -ne "#" }
+  $tweaks | ForEach { Invoke-Expression $_ }
+}
+
+$tweaks = $pp.keys
+foreach ($tweak in $tweaks) {
+  if ($tweak -ne 'restoreall' -and $tweak -ne 'applyall' -and $tweak -ne 'custom') {
+     Invoke-Expression $tweak
+  }
+}
